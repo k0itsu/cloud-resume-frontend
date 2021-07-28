cb7167f000d1e6846845b37228d8afa1a45f669d
Edward Delin <eldelin@gmail.com>
2021-07-28 14:24:38 -0400

    initial commit

---
+++ static/app.js
@@ -0,0 +1,15 @@
+console.log('successfully loaded our js file!')
+
+
+/*
+getting the counter.
+
+we can select the counter id using css selector.
+
+how do we differentiate the different users?
+
+create the API/SAM MODEL for the app now?
+
+
+
+*/
